5fcd2639bcdb926293be0b7eb6b21b80c1f9e498
openXu <openXu@yeah.net>
2016-10-14 17:06:32 +0800

    View的显示过程

--- app/src/main/java/com/openxu/activitylaunch/ShowLayout.java
+++ app/src/main/java/com/openxu/activitylaunch/ShowLayout.java
@@ -14,16 +14,20 @@ import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.Region;
+import android.graphics.drawable.Drawable;
 import android.hardware.display.DisplayManager;
 import android.os.Binder;
 import android.os.Debug;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteException;
+import android.os.Trace;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Choreographer;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.InputQueue;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -40,6 +44,11 @@ import android.view.inputmethod.InputMethodManager;
 
 import com.android.server.am.IWindowSession;
 
+import java.util.ArrayList;
+
+import static android.view.View.MeasureSpec.AT_MOST;
+import static android.view.View.MeasureSpec.EXACTLY;
+import static android.view.View.MeasureSpec.getMode;
 import static android.view.WindowManager.LayoutParams.FIRST_APPLICATION_WINDOW;
 import static android.view.WindowManager.LayoutParams.FIRST_SUB_WINDOW;
 import static android.view.WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
@@ -80,7 +89,6 @@ public class ShowLayout extends Activity{
                 r.window = r.activity.getWindow();
                 //①. 获取activity的顶层视图decor
                 View decor = r.window.getDecorView();
-                //让decor显示出来
                 decor.setVisibility(View.INVISIBLE);
                 //②. 获取WindowManager对象
                 ViewManager wm = a.getWindowManager();
@@ -403,12 +411,12 @@ public class ShowLayout extends Activity{
     }
 
     /**
-     * step5: ViewRootImpl.performTraversals()
+     * step4: ViewRootImpl.performTraversals()
      */
     private void performTraversals() {
         final View host = mView;   //mView就是Activity的根窗口mDecor
         ...
-        //一、下面代码主要确定Activity窗口的大小
+        //下面代码主要确定Activity窗口的大小
         boolean windowSizeMayChange = false;
         //Activity窗口的宽度desiredWindowWidth和高度desiredWindowHeight
         int desiredWindowWidth;
@@ -531,7 +539,7 @@ public class ShowLayout extends Activity{
 
         ...
 
-        /**
+        /*
          * 接下来的两段代码都是在满足下面的条件之一的情况下执行的：
          * 1. Activity窗口是第一次执行测量、布局和绘制操作，即ViewRoot类的成员变量mFirst的值等于true
          * 2. 前面得到的变量windowShouldResize的值等于true，即Activity窗口的大小的确是发生了变化。
@@ -544,7 +552,7 @@ public class ShowLayout extends Activity{
             ...
             try {
 
-                //二、请求WindowManagerService服务计算Activity窗口的大小以及内容区域边衬大小和可见区域边衬大小
+                //★请求WindowManagerService服务计算Activity窗口的大小以及内容区域边衬大小和可见区域边衬大小
                 relayoutResult = relayoutWindow(params, viewVisibility, insetsPending);
                 ...
             } catch (RemoteException e) {
@@ -558,7 +566,6 @@ public class ShowLayout extends Activity{
             }
             ...
 
-            //这段代码用来检查是否需要重新测量Activity窗口的大小
             if (!mStopped || mReportNextDraw) {
                 boolean focusChangedDueToTouchMode = ensureTouchModeLocally(
                         (relayoutResult&WindowManagerGlobal.RELAYOUT_RES_IN_TOUCH_MODE) != 0);
@@ -626,6 +633,373 @@ public class ShowLayout extends Activity{
 
         mIsInTraversal = false;
     }
+
+
+    /**
+     * step5: ViewRootImpl.performMeasure()
+     * 控件测量过程开始
+     */
+    private void performMeasure(int childWidthMeasureSpec, int childHeightMeasureSpec) {
+        Trace.traceBegin(Trace.TRACE_TAG_VIEW, "measure");
+        try {
+            mView.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+        } finally {
+            Trace.traceEnd(Trace.TRACE_TAG_VIEW);
+        }
+    }
+
+
+    /**
+     * step6: View.measure()
+     */
+    public final void measure(int widthMeasureSpec, int heightMeasureSpec) {
+        ...
+        //如果View的mPrivateFlags的PFLAG_FORCE_LAYOUT位不等于0时，就表示当前视图正在请求执行一次布局操作；
+        //或者当前的宽高约束条件不等于视图上一次的约束条件时，需要重新测量View的大小。
+        if ((mPrivateFlags & PFLAG_FORCE_LAYOUT) == PFLAG_FORCE_LAYOUT ||
+                widthMeasureSpec != mOldWidthMeasureSpec ||
+                heightMeasureSpec != mOldHeightMeasureSpec) {
+
+            //首先清除所测量的维度标志
+            mPrivateFlags &= ~PFLAG_MEASURED_DIMENSION_SET;
+
+            ...
+
+            int cacheIndex = (mPrivateFlags & PFLAG_FORCE_LAYOUT) == PFLAG_FORCE_LAYOUT ? -1 :
+                    mMeasureCache.indexOfKey(key);
+            //如果需要强制布局操作，或者忽略测量历史，将会调用onMeasure对控件进行一次测量
+            //sIgnoreMeasureCache是一个boolean值，初始化为sIgnoreMeasureCache = targetSdkVersion < KITKAT;
+            //意思是如果Android版本低于19，每次都会调用onMeasure()，而19以上时sIgnoreMeasureCache==false
+            if (cacheIndex < 0 || sIgnoreMeasureCache) {
+                //调用onMeasure来真正执行测量宽度和高度的操作
+                onMeasure(widthMeasureSpec, heightMeasureSpec);
+                mPrivateFlags3 &= ~PFLAG3_MEASURE_NEEDED_BEFORE_LAYOUT;
+            } else {
+                ...
+            }
+            // flag not set, setMeasuredDimension() was not invoked, we raise
+            // an exception to warn the developer
+            if ((mPrivateFlags & PFLAG_MEASURED_DIMENSION_SET) != PFLAG_MEASURED_DIMENSION_SET) {
+                throw new IllegalStateException("View with id " + getId() + ": "
+                        + getClass().getName() + "#onMeasure() did not set the"
+                        + " measured dimension by calling"
+                        + " setMeasuredDimension()");
+            }
+
+            mPrivateFlags |= PFLAG_LAYOUT_REQUIRED;
+        }
+
+        //保存这次测量的宽高约束
+        mOldWidthMeasureSpec = widthMeasureSpec;
+        mOldHeightMeasureSpec = heightMeasureSpec;
+
+        mMeasureCache.put(key, ((long) mMeasuredWidth) << 32 |
+                (long) mMeasuredHeight & 0xffffffffL); // suppress sign extension
+    }
+
+
+    /**
+     * step7: FrameLayout.onMeasure()
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //获得一个视图容器所包含的子视图的个数
+        int count = getChildCount();
+
+        final boolean measureMatchParentChildren =
+                View.MeasureSpec.getMode(widthMeasureSpec) != View.MeasureSpec.EXACTLY ||
+                        View.MeasureSpec.getMode(heightMeasureSpec) != View.MeasureSpec.EXACTLY;
+        //mMatchParentChildren用于缓存子控件中布局参数设置为填充父窗体的控件
+        mMatchParentChildren.clear();
+
+        int maxHeight = 0;
+        int maxWidth = 0;
+        int childState = 0;
+
+        //①、遍历根窗口下所有的子控件，挨个测量大小
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (mMeasureAllChildren || child.getVisibility() != GONE) {
+                //调用measureChildWithMargins来测量每一个子视图的宽度和高度
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
+                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+                //由于FrameLayout的布局特性（Z轴帧布局，向上覆盖），将子控件中，最大的宽高值记录下来
+                maxWidth = Math.max(maxWidth,
+                        child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
+                maxHeight = Math.max(maxHeight,
+                        child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin);
+                childState = combineMeasuredStates(childState, child.getMeasuredState());
+                if (measureMatchParentChildren) {
+                    //由于现在本容器的宽高都还没有确定下来，子控件设置为填充父窗体肯定没法计算，所以先缓存起来
+                    if (lp.width == LayoutParams.MATCH_PARENT ||
+                            lp.height == LayoutParams.MATCH_PARENT) {
+                        mMatchParentChildren.add(child);
+                    }
+                }
+            }
+        }
+
+        //②、设置FrameLayout的宽高
+        //上面已经得到子控件中宽高的最大值，然后加上容器（FrameLayout）设置的padding值
+        // Account for padding too
+        maxWidth += getPaddingLeftWithForeground() + getPaddingRightWithForeground();
+        maxHeight += getPaddingTopWithForeground() + getPaddingBottomWithForeground();
+
+        //判断是否设置有最小宽度和高度，如果有设置，需要和上面计算的值比较，选择较大的值作为容器宽高
+        // Check against our minimum height and width
+        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
+        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
+
+        //是否设置有前景图，如果有，需要考虑背景的宽高
+        // Check against our foreground's minimum height and width
+        final Drawable drawable = getForeground();
+        if (drawable != null) {
+            maxHeight = Math.max(maxHeight, drawable.getMinimumHeight());
+            maxWidth = Math.max(maxWidth, drawable.getMinimumWidth());
+        }
+        //调用resolveSizeAndState()方法根据计算出的宽高值和宽高约束参数，计算出正确的宽高值；
+        //然后调用setMeasuredDimension()方法设置当前容器的宽高值
+        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
+                resolveSizeAndState(maxHeight, heightMeasureSpec,
+                        childState << MEASURED_HEIGHT_STATE_SHIFT));
+
+        //③、计算子控件中设置为填充父窗体的控件的大小
+        count = mMatchParentChildren.size();
+        if (count > 1) {
+            for (int i = 0; i < count; i++) {
+                final View child = mMatchParentChildren.get(i);
+                final ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) child.getLayoutParams();
+
+                final int childWidthMeasureSpec;
+                if (lp.width == LayoutParams.MATCH_PARENT) {
+                    final int width = Math.max(0, getMeasuredWidth()
+                            - getPaddingLeftWithForeground() - getPaddingRightWithForeground()
+                            - lp.leftMargin - lp.rightMargin);
+                    childWidthMeasureSpec = View.MeasureSpec.makeMeasureSpec(
+                            width, View.MeasureSpec.EXACTLY);
+                } else {
+                    childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec,
+                            getPaddingLeftWithForeground() + getPaddingRightWithForeground() +
+                                    lp.leftMargin + lp.rightMargin,
+                            lp.width);
+                }
+
+                final int childHeightMeasureSpec;
+                if (lp.height == LayoutParams.MATCH_PARENT) {
+                    final int height = Math.max(0, getMeasuredHeight()
+                            - getPaddingTopWithForeground() - getPaddingBottomWithForeground()
+                            - lp.topMargin - lp.bottomMargin);
+                    childHeightMeasureSpec = View.MeasureSpec.makeMeasureSpec(
+                            height, View.MeasureSpec.EXACTLY);
+                } else {
+                    childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec,
+                            getPaddingTopWithForeground() + getPaddingBottomWithForeground() +
+                                    lp.topMargin + lp.bottomMargin,
+                            lp.height);
+                }
+
+                child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+            }
+        }
+    }
+
+    /**
+     * step8: ViewRootImpl.performLayout()
+     * 窗口布局layout过程
+     */
+    private void performLayout(WindowManager.LayoutParams lp, int desiredWindowWidth,
+                               int desiredWindowHeight) {
+        mLayoutRequested = false;
+        mScrollMayChange = true;
+        mInLayout = true;
+        final View host = mView;
+        ...
+
+        Trace.traceBegin(Trace.TRACE_TAG_VIEW, "layout");
+        try {
+            host.layout(0, 0, host.getMeasuredWidth(), host.getMeasuredHeight());
+            ...
+        } finally {
+            Trace.traceEnd(Trace.TRACE_TAG_VIEW);
+        }
+        mInLayout = false;
+     }
+
+    /**
+     * step9: View.layout()
+     */
+    public void layout(int l, int t, int r, int b) {
+        if ((mPrivateFlags3 & PFLAG3_MEASURE_NEEDED_BEFORE_LAYOUT) != 0) {
+            onMeasure(mOldWidthMeasureSpec, mOldHeightMeasureSpec);
+            mPrivateFlags3 &= ~PFLAG3_MEASURE_NEEDED_BEFORE_LAYOUT;
+        }
+
+        //记录上一次布局后的左上右下的坐标值
+        int oldL = mLeft;
+        int oldT = mTop;
+        int oldB = mBottom;
+        int oldR = mRight;
+
+        //为控件重新设置新的坐标值，并判断是否需要重新布局
+        boolean changed = isLayoutModeOptical(mParent) ?
+                setOpticalFrame(l, t, r, b) : setFrame(l, t, r, b);
+
+        if (changed || (mPrivateFlags & PFLAG_LAYOUT_REQUIRED) == PFLAG_LAYOUT_REQUIRED) {
+            //onLayout()方法在View中是一个空实现，各种容器需要重写onLayout()方法，为子控件布局
+            onLayout(changed, l, t, r, b);
+            mPrivateFlags &= ~PFLAG_LAYOUT_REQUIRED;
+
+            ListenerInfo li = mListenerInfo;
+            if (li != null && li.mOnLayoutChangeListeners != null) {
+                ArrayList<OnLayoutChangeListener> listenersCopy =
+                        (ArrayList<OnLayoutChangeListener>)li.mOnLayoutChangeListeners.clone();
+                int numListeners = listenersCopy.size();
+                for (int i = 0; i < numListeners; ++i) {
+                    listenersCopy.get(i).onLayoutChange(this, l, t, r, b, oldL, oldT, oldR, oldB);
+                }
+            }
+        }
+
+        mPrivateFlags &= ~PFLAG_FORCE_LAYOUT;
+        mPrivateFlags3 |= PFLAG3_IS_LAID_OUT;
+    }
+
+    /**
+     * step10：DecorView .onLayout()
+     */
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        getOutsets(mOutsets);
+        if (mOutsets.left > 0) {
+            offsetLeftAndRight(-mOutsets.left);
+        }
+        if (mOutsets.top > 0) {
+            offsetTopAndBottom(-mOutsets.top);
+        }
+    }
+
+
+    /**
+     * step11：FrameLayout .onLayout()
+     */
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        layoutChildren(left, top, right, bottom, false /* no force left gravity */);
+    }
+
+    void layoutChildren(int left, int top, int right, int bottom,
+                        boolean forceLeftGravity) {
+        //获取子控件数量
+        final int count = getChildCount();
+
+        //获取padding值
+        final int parentLeft = getPaddingLeftWithForeground();
+        final int parentRight = right - left - getPaddingRightWithForeground();
+
+        final int parentTop = getPaddingTopWithForeground();
+        final int parentBottom = bottom - top - getPaddingBottomWithForeground();
+        //遍历子控件，为其计算左上右下坐标，由于不同容器的布局特性，下面的计算过程都是根据容器的布局特性计算的
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+                final int width = child.getMeasuredWidth();
+                final int height = child.getMeasuredHeight();
+
+                int childLeft;
+                int childTop;
+
+                int gravity = lp.gravity;
+                if (gravity == -1) {
+                    gravity = DEFAULT_CHILD_GRAVITY;
+                }
+
+                final int layoutDirection = getLayoutDirection();
+                final int absoluteGravity = Gravity.getAbsoluteGravity(gravity, layoutDirection);
+                final int verticalGravity = gravity & Gravity.VERTICAL_GRAVITY_MASK;
+
+                switch (absoluteGravity & Gravity.HORIZONTAL_GRAVITY_MASK) {
+                    case Gravity.CENTER_HORIZONTAL:
+                        //如果设置gravity是水平居中
+                        childLeft = parentLeft + (parentRight - parentLeft - width) / 2 +
+                                lp.leftMargin - lp.rightMargin;
+                        break;
+                    case Gravity.RIGHT:
+                        //如果是靠右
+                        if (!forceLeftGravity) {
+                            childLeft = parentRight - width - lp.rightMargin;
+                            break;
+                        }
+                    case Gravity.LEFT:
+                    default:
+                        //如果是靠左或者默认
+                        childLeft = parentLeft + lp.leftMargin;
+                }
+
+                switch (verticalGravity) {
+                    case Gravity.TOP:
+                        childTop = parentTop + lp.topMargin;
+                        break;
+                    case Gravity.CENTER_VERTICAL:
+                        childTop = parentTop + (parentBottom - parentTop - height) / 2 +
+                                lp.topMargin - lp.bottomMargin;
+                        break;
+                    case Gravity.BOTTOM:
+                        childTop = parentBottom - height - lp.bottomMargin;
+                        break;
+                    default:
+                        childTop = parentTop + lp.topMargin;
+                }
+                //调用其layout()方法为子控件设置坐标
+                child.layout(childLeft, childTop, childLeft + width, childTop + height);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
